b691511f267ffa48f836a6513522f8f55984702a
adisa39 <yusufhammed39@gmail.com>
2025-08-21 19:19:42 +0100

    modify appContext to include user authentication

--- src/providers/AppContextProvider.tsx
+++ src/providers/AppContextProvider.tsx
@@ -8,20 +8,19 @@ import {
   ReactNode,
   useEffect
 } from 'react';
-
-import { Pi } from '@pinetwork-js/sdk';
-import axiosClient, {setAuthToken} from '@/config/client';
+import axiosClient, { setAuthToken } from '@/config/client';
 import { onIncompletePaymentFound } from '@/config/payment';
-import { AuthResult } from '@/constants/pi';
+import { AuthResult } from '../constants/pi';
 import { IUser } from '@/constants/types';
 
-// import console from '../console.config.mjs';
+import logger from '../../logger.config.mjs';
 
 interface IAppContextProps {
   currentUser: IUser | null;
   setCurrentUser: React.Dispatch<SetStateAction<IUser | null>>;
+  walletAddress: string | null
   registerUser: () => void;
-  autoLoginUser: ()=> void;
+  autoLoginUser: () => void;
   isSigningInUser: boolean;
   reload: boolean;
   alertMessage: string | null;
@@ -30,13 +29,17 @@ interface IAppContextProps {
   setReload: React.Dispatch<SetStateAction<boolean>>;
   isSaveLoading: boolean;
   setIsSaveLoading: React.Dispatch<SetStateAction<boolean>>;
+  adsSupported: boolean;
+  toggleNotification: boolean;
+  setToggleNotification: React.Dispatch<SetStateAction<boolean>>;
 }
 
 const initialState: IAppContextProps = {
   currentUser: null,
   setCurrentUser: () => {},
-  registerUser: () => { },
-  autoLoginUser: ()=> {},
+  walletAddress: '',
+  registerUser: () => {},
+  autoLoginUser: () => {},
   isSigningInUser: false,
   reload: false,
   alertMessage: null,
@@ -44,7 +47,10 @@ const initialState: IAppContextProps = {
   showAlert: () => {},
   setReload: () => {},
   isSaveLoading: false,
-  setIsSaveLoading: () => {}
+  setIsSaveLoading: () => {},
+  adsSupported: false,
+  toggleNotification: false,
+  setToggleNotification: () => {},
 };
 
 export const AppContext = createContext<IAppContextProps>(initialState);
@@ -55,11 +61,13 @@ interface AppContextProviderProps {
 
 const AppContextProvider = ({ children }: AppContextProviderProps) => {
   const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+  const [walletAddress, setWalletAddress] = useState<string | null>(null);
   const [isSigningInUser, setIsSigningInUser] = useState(false);
   const [reload, setReload] = useState(false);
   const [isSaveLoading, setIsSaveLoading] = useState(false);
-
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
+  const [adsSupported, setAdsSupported] = useState(false); 
+  const [toggleNotification, setToggleNotification] = useState<boolean>(true);
 
   const showAlert = (message: string) => {
     setAlertMessage(message);
@@ -68,83 +76,123 @@ const AppContextProvider = ({ children }: AppContextProviderProps) => {
     }, 5000);
   };
 
+    const loadPiSdk = (): Promise<typeof window.Pi> => {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://sdk.minepi.com/pi-sdk.js';
+      script.async = true;
+      script.onload = () => resolve(window.Pi);
+      script.onerror = () => reject(new Error('Failed to load Pi SDK'));
+      document.head.appendChild(script);
+    });
+  };
+
+  /* Register User via Pi SDK */
   const registerUser = async () => {
-    console.info('Initializing Pi SDK for user registration.');
-    await Pi.init({ version: '2.0', sandbox: process.env.NODE_ENV === 'development' });
-    let isInitiated = Pi.initialized;
+    logger.info('Starting user registration.');
 
-    if (isInitiated) {
-      try {
-        setIsSigningInUser(true);
-        const pioneerAuth: AuthResult = await window.Pi.authenticate([
-          'username', 
-          'payments', 
-          'wallet_address'
-        ], onIncompletePaymentFound);
-        const res = await axiosClient.post("/users/authenticate", 
-          {}, // empty body
-          {
-            headers: {
-              Authorization: `Bearer ${pioneerAuth.accessToken}`,
-            },
-          }
-        );
-
-        if (res.status === 200) {
-          setAuthToken(res.data?.token);
-          setCurrentUser(res.data.user);
-          console.info('User authenticated successfully.');
-          setTimeout(() => {
-            setIsSigningInUser(false); // hide the splash screen after the delay
-          }, 2500);
-        } else if (res.status === 500) {
-          setCurrentUser(null);
-          console.error('User authentication failed.');
-          setIsSigningInUser(false);
-        }        
-      } catch (error) {
-        console.error('Error during user registration:', error);
-        setIsSigningInUser(false);
+    // Ensure Pi SDK is loaded and initialized
+    if (typeof window !== 'undefined') {
+      if (!window.Pi || !window.Pi.initialized) {
+        try {
+          const Pi = await loadPiSdk();
+          Pi.init({ version: '2.0', sandbox: process.env.NODE_ENV === 'development' });
+        } catch (err) {
+          logger.error('Failed to load or initialize Pi SDK:', err);
+          return;
+        }
       }
-    } else {
-      console.error('PI SDK failed to initialize.');
+
+      if (window.Pi.initialized) {
+        try {
+          setIsSigningInUser(true);
+          const pioneerAuth: AuthResult = await window.Pi.authenticate(
+            ['username', 'payments', 'wallet_address'],
+            onIncompletePaymentFound
+          );
+          // setWalletAddress(pioneerAuth.accessToken)
+          logger.info("User authenticated with Pi SDK:", {pioneerAuth}); 
+
+          // Send accessToken to backend
+          const res = await axiosClient.post(
+            "/users/authenticate",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${pioneerAuth.accessToken}`,
+              },
+            }
+          );
+
+          if (res.status === 200) {
+            setAuthToken(res.data.token);
+            setCurrentUser(res.data.user);
+            const authUser: IUser = res.data;
+            logger.info(`User authenticated successfully.`, {authUser});
+          } else {
+            setCurrentUser(null);
+            logger.error('User authentication failed.');
+          }
+        } catch (error) {
+          logger.error('Error during user registration:', error);
+        }  finally {
+          setTimeout(() => setIsSigningInUser(false), 3000);
+        }
+      } else {
+        logger.error('PI SDK failed to initialize.');
+      }
     }
   };
 
+  /* Attempt Auto Login (fallback to Pi auth) */
   const autoLoginUser = async () => {
-    console.info('Attempting to auto-login user.');
+    logger.info('Attempting to auto-login user.');
     try {
       setIsSigningInUser(true);
       const res = await axiosClient.get('/users/me');
 
       if (res.status === 200) {
-        console.info('Auto-login successful.');
-        setCurrentUser(res.data);
-        setTimeout(() => {
-          setIsSigningInUser(false); // hide the splash screen after the delay
-        }, 2500);
+        logger.info('Auto-login successful.');
+        setCurrentUser(res.data.user);
       } else {
+        logger.warn('Auto-login failed.');
         setCurrentUser(null);
-        console.warn('Auto-login failed.');
-        setIsSigningInUser(false);
       }
     } catch (error) {
-      console.error('Auto login unresolved; attempting Pi SDK authentication:', error);
+      logger.error('Auto login unresolved; attempting Pi SDK authentication:', error);
       await registerUser();
+    } finally {
+      setTimeout(() => setIsSigningInUser(false), 2500);
     }
-  }
+  };
 
   useEffect(() => {
-    console.info('AppContextProvider mounted.');
-    if (!currentUser) {
-      registerUser();
-    } else {
-      autoLoginUser();
-    }
+    logger.info('AppContextProvider mounted.');
+
+    autoLoginUser();
   }, []);
 
   return (
-    <AppContext.Provider value={{ currentUser, setCurrentUser, registerUser, autoLoginUser, isSigningInUser, reload, setReload, showAlert, alertMessage, setAlertMessage, isSaveLoading, setIsSaveLoading }}>
+    <AppContext.Provider 
+      value={{ 
+        currentUser, 
+        setCurrentUser, 
+        walletAddress,
+        registerUser, 
+        autoLoginUser, 
+        isSigningInUser, 
+        reload, 
+        setReload, 
+        showAlert, 
+        alertMessage, 
+        setAlertMessage, 
+        isSaveLoading, 
+        setIsSaveLoading, 
+        adsSupported,
+        toggleNotification,
+        setToggleNotification
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
